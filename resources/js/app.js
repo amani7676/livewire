e874a4d77aec019e4c7531570d3e7f1e2114fd13
farshid amani <f.amani.dev@gmail.com>
2025-05-18 07:42:57 +0330

    صث

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,15 @@
-import './bootstrap';
+// ابتدا jQuery را import کنید
+import $ from 'jquery';
+window.$ = window.jQuery = $;
 
+// سپس بقیه کتابخانه‌ها
+import 'bootstrap';
 import Alpine from 'alpinejs';
+import Swal from 'sweetalert2';
 
+// راه‌اندازی Alpine
 window.Alpine = Alpine;
-
 Alpine.start();
+
+// اگر فایل bootstrap.js دارید
+import './bootstrap';
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
-    @vite('resources/css/app.css')
-</head>
-<body>
-    
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
-    @vite(['resources/js/app.js', 'resources/js/alert.js'])
+</head>
+
+<body>
+
+    {{-- <script src="{{ asset('dist/app.js') }}"></script> --}}
+    <div x-data="dropdown">
+        <button @click="toggle">Expand</button>
+
+        <span x-show="open">Content...</span>
+    </div>
+
+    <div x-data="dropdown">
+        <button @click="toggle">Expand</button>
+
+        <span x-show="open">Some Other Content...</span>
+    </div>
+
+    <script>
+        Alpine.data('dropdown', () => ({
+            open: false,
+
+            toggle() {
+                this.open = !this.open
+            }
+        }))
+    </script>
 </body>
+
 </html>
